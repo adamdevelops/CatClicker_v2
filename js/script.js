bab265da5eb83b42d8223a12f439217bfff19c7c
Adam <adamh93@verizon.net>
2017-01-18 21:30:45 -0500

    chore: Add README.md file fix: Added eventListners to the images themseleves and removed any additional parentheses in the addUp functions

--- js/script.js
+++ js/script.js
@@ -3,29 +3,25 @@ var count2 = 0;
 
 
 //Cat 1
-var addUp1 = (function() {
+var addUp1 = function() {
+  count1 += 1;
+  document.getElementById("counter1").innerHTML = "This has been clicked " + count1 + " times.";
 
-  return function () {
-    var element1 = document.getElementById("counter1");
-    if (element1) element1.innerHTML = ++count1;
-    document.getElementById("counter1").innerHTML = "This has been clicked " + count1 + " times.";
-  }
-}());
+};
 
 
 // Attach as a listener
-document.addEventListener("click", addUp1, false);
+//el.addEventListener("click", function(){modifyText("four")}, false);
+document.getElementById("cat1").addEventListener("click", addUp1, false);
+
 
 //Cat 2
-var addUp2 = (function() {
+var addUp2 = function() {
+  count2 += 1;
+  document.getElementById("counter2").innerHTML = "This has been clicked " + count2 + " times.";
 
-  return function () {
-    var element2 = document.getElementById("counter2");
-    if (element2) element2.innerHTML = ++count2;
-    document.getElementById("counter2").innerHTML = "This has been clicked " + count2 + " times.";
-  }
-}());
+};
 
 
 // Attach as a listener
-document.addEventListener("click", addUp2, false);
+document.getElementById("cat2").addEventListener("click", addUp2, false);
